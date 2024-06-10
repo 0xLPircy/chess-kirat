eab5914a357d5655b223fb090bd5dbc353b79cee
L-Pircy <taveesha.agarwal@gmail.com>
2024-06-11 00:45:48 +0530

    be

--- backend1/src/Game.ts
+++ backend1/src/Game.ts
@@ -1,26 +1,89 @@
 import { WebSocket } from "ws";
+import { Chess } from "chess.js";
+import { GAME_OVER, INIT_GAME, MOVE } from "./messages";
 
 export class Game {
   public player1: WebSocket;
   public player2: WebSocket;
-  private board: string;
+  public board: Chess;
   private moves: string[];
   private startTime: Date;
 
   constructor(player1: WebSocket, player2: WebSocket) {
     this.player1 = player1;
     this.player2 = player2;
-    this.board = "";
+    this.board = new Chess();
     this.moves = [];
     this.startTime = new Date();
+    this.player1.send(
+      JSON.stringify({
+        type: INIT_GAME,
+        payload: {
+          color: "white",
+        },
+      })
+    );
+    this.player2.send(
+      JSON.stringify({
+        type: INIT_GAME,
+        payload: {
+          color: "black",
+        },
+      })
+    );
   }
 
-  makeMove(socket: WebSocket, move: string) {
-    // Validation:
-    // user ka move?
-    // move valid?
-    // updateBoard
-    // push move
+  makeMove(
+    socket: WebSocket,
+    move: {
+      from: string;
+      to: string;
+    }
+  ) {
+    if (this.board.moves.length % 2 === 0 && socket !== this.player1) {
+      return;
+    }
+    if (this.board.moves.length % 2 === 1 && socket !== this.player2) {
+      return;
+    }
+    try {
+      this.board.move(move);
+    } catch (err) {
+      return;
+    }
+    if (this.board.isGameOver()) {
+      this.player1.emit(
+        JSON.stringify({
+          type: GAME_OVER,
+          payload: {
+            winner: this.board.turn() === "w" ? "black" : "white",
+          },
+        })
+      );
+      this.player2.emit(
+        JSON.stringify({
+          type: GAME_OVER,
+          payload: {
+            winner: this.board.turn() === "w" ? "black" : "white",
+          },
+        })
+      );
+    }
+    if (this.board.moves.length % 2 === 0) {
+      this.player2.emit(
+        JSON.stringify({
+          type: MOVE,
+          payload: move,
+        })
+      );
+    } else {
+      this.player1.emit(
+        JSON.stringify({
+          type: MOVE,
+          paload: move,
+        })
+      );
+    }
     // check game end
     // send uodated boar dot both players
   }
